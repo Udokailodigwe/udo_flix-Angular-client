803f6cd070048701e9ac42e515084e8b1d18bf6c
Udokailodigwe <ilodigweudoka@gmail.com>
2022-04-14 03:20:33 +0300

    Imported login form component and created open dialog function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udo_flix-Angular-client';
+
+  constructor(
+    public dialog: MatDialog
+  ) {}
+//open up dialog when sign up button is clicked
+  openRegistrationDialog() {
+    this.dialog.open(RegistrationFormComponent, {
+      width: '300px',
+      //panelClass: 'myClass' 
+    });
+  }
+  //open up dialog when login button is clicked
+  openLoginDialog () {
+    this.dialog.open(LoginFormComponent, {
+      width: '300px',
+      //panelClass: 'myClass' 
+    }) 
+  }
 }
