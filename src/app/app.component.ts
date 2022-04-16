473c8276e8251f67ad47d516645590678b89fb7c
Udokailodigwe <ilodigweudoka@gmail.com>
2022-04-16 03:32:48 +0300

    Modified app root template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
-
 
 @Component({
   selector: 'app-root',
@@ -12,21 +8,4 @@ import { RegistrationFormComponent } from './registration-form/registration-form
 export class AppComponent {
   title = 'udo_flix-Angular-client';
 
-  constructor(
-    public dialog: MatDialog
-  ) {}
-//open up dialog when sign up button is clicked
-  openRegistrationDialog() {
-    this.dialog.open(RegistrationFormComponent, {
-      width: '300px',
-      //panelClass: 'myClass' 
-    });
-  }
-  //open up dialog when login button is clicked
-  openLoginDialog () {
-    this.dialog.open(LoginFormComponent, {
-      width: '300px',
-      //panelClass: 'myClass' 
-    }) 
-  }
 }
