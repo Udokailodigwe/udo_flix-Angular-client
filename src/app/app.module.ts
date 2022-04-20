ca168b9601a6ac8000156abdb538ef85ed157ae0
Udokailodigwe <ilodigweudoka@gmail.com>
2022-04-20 13:23:24 +0300

    Imported newly created components, and included their route paths. Imported additional angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+
+/**angular material */
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -9,19 +10,35 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
+
+/**for server connection */
+import {HttpClientModule} from '@angular/common/http';
+
+/**app router */
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/**app components */
+import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FavoriteMoviesComponent } from './favorite-movies/favorite-movies.component';
+import { GenreComponent } from './genre/genre.component';
+import { DirectorComponent } from './director/director.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
 
 const appRoutes: Routes = [
   {path: 'welcome', component: WelcomePageComponent},
+  {path: 'profile', component: UserProfileComponent},
   {path: 'movies', component: MovieCardComponent},
+  {path: 'favorite', component: FavoriteMoviesComponent},
   {path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
 ];
 
@@ -31,7 +48,13 @@ const appRoutes: Routes = [
     RegistrationFormComponent,
     LoginFormComponent,
     MovieCardComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    UserProfileComponent,
+    NavigationComponent,
+    FavoriteMoviesComponent,
+    GenreComponent,
+    DirectorComponent,
+    SynopsisComponent
   ],
   imports: [
     BrowserModule,
@@ -46,6 +69,8 @@ const appRoutes: Routes = [
     MatFormFieldModule,
     MatCardModule,
     MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
     RouterModule.forRoot(appRoutes)
 
   ],
